c783957538ccb77cf573f3a2ad6cb780afca3262
PrimusAbOris <guy.ross.rit@gmail.com>
2023-09-02 23:33:22 -0400

    Create GenericFactoryApplication.java

---
+++ src/com/coderscampus/lesson2/GenericFactoryApplication.java
@@ -0,0 +1,17 @@
+package com.coderscampus.lesson2;
+
+public class GenericFactoryApplication {
+
+	public static void main(String[] args) {
+		/* Abstract classes:
+		 * When you need to create a lot of similar objects,
+		 * an abstract class is good to have as a parent class for them
+		 * 
+		 * 
+		 * Less restricted than interfaces; can have overriding methods
+		 */
+ 
+
+	}
+
+}
